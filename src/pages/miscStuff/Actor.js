5b570bbdbf849707fa1ddf90ef744ab556ca8218
anthonyMarra <marraanthony4@gmail.com>
2022-11-29 20:31:30 -0500

    finish part 3

--- src/pages/miscStuff/Actor.js
+++ src/pages/miscStuff/Actor.js
@@ -4,6 +4,8 @@ function Actor({ actor }) {
     return (
         <>
             <h4>{actor}</h4>
+            <img src="https://picsum.photos/200/300" alt="alternatetext"></img>
+            {/* no clue how to get many random images lol */}
         </>
     );
 }
